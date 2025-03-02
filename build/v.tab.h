7adb9e707a1842d39fc2594e46734cf8bd1e7c4f
carlos-burelo <carlosfernandoburelo@gmail.com>
2025-03-02 12:48:22 -0600

    traduccion de tokens

--- build/v.tab.h
+++ build/v.tab.h
@@ -1,23 +1,22 @@
+/* A Bison parser, made by GNU Bison 3.8.2.  */
 
-/* A Bison parser, made by GNU Bison 2.4.1.  */
+/* Bison interface for Yacc-like parsers in C
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
-   
-      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
-   
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Inc.
+
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -28,61 +27,75 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-   
+
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
 
-/* Tokens.  */
-#ifndef YYTOKENTYPE
-# define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     NUMBER = 258,
-     VARIABLE = 259,
-     STRING = 260,
-     WHILE = 261,
-     FOR = 262,
-     IF = 263,
-     THEN = 264,
-     PRINT = 265,
-     ASSIGN = 266,
-     EXIT = 267,
-     RANDOM = 268,
-     PI = 269,
-     SCAN = 270,
-     LOG = 271,
-     EXP = 272,
-     SQRT = 273,
-     FLOOR = 274,
-     CEIL = 275,
-     ABS = 276,
-     SIN = 277,
-     ASIN = 278,
-     COS = 279,
-     ACOS = 280,
-     TAN = 281,
-     ATAN = 282,
-     IFX = 283,
-     ELSE = 284,
-     OR = 285,
-     AND = 286,
-     NE = 287,
-     LE = 288,
-     GE = 289,
-     NOT = 290,
-     UMINUS = 291
-   };
+#ifndef YY_YY_V_TAB_H_INCLUDED
+# define YY_YY_V_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
 #endif
 
+/* Token kinds.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    NUMBER = 258,                  /* NUMBER  */
+    VARIABLE = 259,                /* VARIABLE  */
+    STRING = 260,                  /* STRING  */
+    WHILE = 261,                   /* WHILE  */
+    FOR = 262,                     /* FOR  */
+    IF = 263,                      /* IF  */
+    THEN = 264,                    /* THEN  */
+    PRINT = 265,                   /* PRINT  */
+    ASSIGN = 266,                  /* ASSIGN  */
+    EXIT = 267,                    /* EXIT  */
+    RANDOM = 268,                  /* RANDOM  */
+    PI = 269,                      /* PI  */
+    SCAN = 270,                    /* SCAN  */
+    LOG = 271,                     /* LOG  */
+    EXP = 272,                     /* EXP  */
+    SQRT = 273,                    /* SQRT  */
+    FLOOR = 274,                   /* FLOOR  */
+    CEIL = 275,                    /* CEIL  */
+    ABS = 276,                     /* ABS  */
+    SIN = 277,                     /* SIN  */
+    ASIN = 278,                    /* ASIN  */
+    COS = 279,                     /* COS  */
+    ACOS = 280,                    /* ACOS  */
+    TAN = 281,                     /* TAN  */
+    ATAN = 282,                    /* ATAN  */
+    IFX = 283,                     /* IFX  */
+    ELSE = 284,                    /* ELSE  */
+    AND = 285,                     /* AND  */
+    OR = 286,                      /* OR  */
+    GE = 287,                      /* GE  */
+    LE = 288,                      /* LE  */
+    NE = 289,                      /* NE  */
+    NOT = 290,                     /* NOT  */
+    UMINUS = 291                   /* UMINUS  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
+#endif
 
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
+union YYSTYPE
 {
-
-/* Line 1676 of yacc.c  */
 #line 24 "v.y"
 
     double dValue;
@@ -90,16 +103,19 @@ typedef union YYSTYPE
     char *vName;
     nodeType *nPtr;
 
+#line 107 "v.tab.h"
 
-
-/* Line 1676 of yacc.c  */
-#line 97 "v.tab.h"
-} YYSTYPE;
+};
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 #endif
+
 
 extern YYSTYPE yylval;
 
 
+int yyparse (void);
+
+
+#endif /* !YY_YY_V_TAB_H_INCLUDED  */
